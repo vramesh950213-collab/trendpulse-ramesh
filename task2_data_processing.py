7898c2c60df6184874cf861847de808657ff3cb4
vramesh950213-collab <v.ramesh950213@gmail.com>
2026-08-24 12:30:15 +0530

    Update task2_data_processing.py

--- task2_data_processing.py
+++ task2_data_processing.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import os
 
 input_file="data/trends_20260823.json"
 
@@ -20,6 +19,13 @@ print(f"After removing nulls: {len(df)}")
 df["score"] = pd.to_numeric(df["score"], errors="coerce")
 
 df["num_comments"]=pd.to_numeric(df["num_comments"], errors="coerce")
+
+df = df.dropna(
+    subset=["score","num_comments"]
+)
+
+df["score"] = df["score"].astype(int) 
+df["num_comments"] = df["num_comments"].astype(int)
 
 df = df[df["score"]>=5]
 
